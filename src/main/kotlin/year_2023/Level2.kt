900b15b8ac82f85830bf7792c3cd5fdab487d61c
Ronny Remesnik <ronny.remesnik@autodesk.com>
2024-11-04 22:25:44 +0200

    Move everything to the class

--- src/main/kotlin/year_2023/Level2.kt
+++ src/main/kotlin/year_2023/Level2.kt
@@ -2,52 +2,53 @@ package year_2023
 
 import Base2023
 
-enum class Color(val color: String, val maxPossible: Int) {
-    GREEN("green", 13),
-    BLUE("blue", 14),
-    RED("red", 12);
-
-    companion object {
-        fun fromString(value: String): Color {
-            return Color.values().first { it.color == value }
-        }
-    }
-}
-
-data class ColorPair(val color: Color, val amountOfBalls: Int)
-data class GameSet(val set: Set<ColorPair>) {
-    fun isValid(): Boolean {
-        return set.all { it.amountOfBalls <= it.color.maxPossible }
-    }
-}
-
-data class Game(val index: Int, val sets: List<GameSet>) {
-    fun isValid(): Boolean {
-        return sets.all { it.isValid() }
-    }
-}
-
-private fun lineToGame(line: String): Game {
-    val gameSplit = line.split(":")
-    val gameIndexData = gameSplit[0]
-    val gameData = gameSplit[1]
-    val gameIndex = gameIndexData.split(" ")[1].toInt()
-    val gameSets = gameData.split(";")
-    val gameSetsList = gameSets.map { set ->
-        val pairs = set.split(",")
-        val pairsList = pairs.map { pair ->
-            val pairSplit = pair.trim().split(" ")
-            val amount = pairSplit[0].toInt()
-            val color = Color.fromString(pairSplit[1])
-            ColorPair(color = color, amountOfBalls = amount)
-        }.toSet()
-        GameSet(pairsList)
-    }
-
-    return Game(gameIndex, gameSetsList)
-}
 
 class Level2 : Base2023(2) {
+    enum class Color(val color: String, val maxPossible: Int) {
+        GREEN("green", 13),
+        BLUE("blue", 14),
+        RED("red", 12);
+
+        companion object {
+            fun fromString(value: String): Color {
+                return Color.values().first { it.color == value }
+            }
+        }
+    }
+
+    data class ColorPair(val color: Color, val amountOfBalls: Int)
+    data class GameSet(val set: Set<ColorPair>) {
+        fun isValid(): Boolean {
+            return set.all { it.amountOfBalls <= it.color.maxPossible }
+        }
+    }
+
+    data class Game(val index: Int, val sets: List<GameSet>) {
+        fun isValid(): Boolean {
+            return sets.all { it.isValid() }
+        }
+    }
+
+    private fun lineToGame(line: String): Game {
+        val gameSplit = line.split(":")
+        val gameIndexData = gameSplit[0]
+        val gameData = gameSplit[1]
+        val gameIndex = gameIndexData.split(" ")[1].toInt()
+        val gameSets = gameData.split(";")
+        val gameSetsList = gameSets.map { set ->
+            val pairs = set.split(",")
+            val pairsList = pairs.map { pair ->
+                val pairSplit = pair.trim().split(" ")
+                val amount = pairSplit[0].toInt()
+                val color = Color.fromString(pairSplit[1])
+                ColorPair(color = color, amountOfBalls = amount)
+            }.toSet()
+            GameSet(pairsList)
+        }
+
+        return Game(gameIndex, gameSetsList)
+    }
+
     override fun part1() {
         val sum = lines.map { lineToGame(it) }
             .filter { it.isValid() }
