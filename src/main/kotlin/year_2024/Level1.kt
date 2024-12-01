e1db73293d77b77e8a5e073c0070d7ea5f32259d
Ronny Remesnik <ronny.remesnik@autodesk.com>
2024-12-01 20:55:26 +0200

    Fix the second list

--- src/main/kotlin/year_2024/Level1.kt
+++ src/main/kotlin/year_2024/Level1.kt
@@ -28,7 +28,6 @@ class Level1 : Base2024(1) {
     override fun part2() {
         val first: MutableList<Int> = mutableListOf()
         val second: MutableList<Int> = mutableListOf()
-        val secondMap: MutableMap<Int, Int> = mutableMapOf()
 
         lines.forEach { line ->
             val splitted = line.split(" ").filter { it.isNotEmpty() }
@@ -36,21 +35,13 @@ class Level1 : Base2024(1) {
             second.add(splitted[1].trim().toInt())
         }
 
-        second.forEach { num ->
-            if (secondMap[num] == null) {
-                secondMap[num] = 1
-            } else {
-                secondMap[num] = secondMap[num]!! + 1
-            }
-        }
+        val countMap = second.groupingBy { it }.eachCount()
 
         val secondSum = first.sumOf {
-            val count = secondMap[it] ?: 0
-            it * count
+            it * countMap.getOrDefault(it, 0)
         }
 
         println("Similarity Score is $secondSum")
-
     }
 
 }
