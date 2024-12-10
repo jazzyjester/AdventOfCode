7251f94ff89befa94b728ff6dacb81c466953c33
Ronny Remesnik <ronny.remesnik@autodesk.com>
2024-12-10 22:25:17 +0200

    6, part 2 Very hard, was stuck on it 4 days

--- src/main/kotlin/year_2024/Level6.kt
+++ src/main/kotlin/year_2024/Level6.kt
@@ -35,7 +35,6 @@ class Level6 : Base2024(6) {
             }
         }
 
-        println(head)
         points.add(head)
         while (true) {
             val destination = Point(head.x + dir.x, head.y + dir.y)
@@ -52,9 +51,79 @@ class Level6 : Base2024(6) {
         }
 
         println("Total distinct positions ${points.size}")
+    }
+
+    data class PointDirection(val point: Point, val dir: Dir)
+
+    private fun runWalkAndCheckExist(obstacle: Point): Boolean {
+        var dir = Dir.UP
+        var head = Point(-1, -1)
+        val points: MutableSet<PointDirection> = mutableSetOf()
+        for (lineIndex in lines.indices) {
+            for (charIndex in lines[lineIndex].indices) {
+                if (lines[lineIndex][charIndex] == '^') {
+                    head = Point(charIndex, lineIndex)
+                    break
+                }
+                if (head.x != -1 && head.y != -1) {
+                    break
+                }
+            }
+        }
+
+        points.add(PointDirection(head, dir))
+        while (true) {
+            val destination = Point(head.x + dir.x, head.y + dir.y)
+            if (destination.x == -1 || destination.x == lines.size || destination.y == -1 || destination.y == lines.size) {
+                return false
+            }
+            if (lines[destination.y][destination.x] == '#' || destination == obstacle) {
+                // obstacle
+                dir = dir.next()
+            } else {
+                head = destination
+                if (points.contains(PointDirection(head, dir))) {
+                    // Loop, exist in previous points
+                    return true
+                }
+                points.add(PointDirection(head, dir))
+            }
+        }
 
     }
 
     override fun part2() {
+        val points: MutableSet<PointDirection> = mutableSetOf()
+        var dir = Dir.UP
+        var head = Point(-1, -1)
+        for (lineIndex in lines.indices) {
+            for (charIndex in lines[lineIndex].indices) {
+                if (lines[lineIndex][charIndex] == '^') {
+                    head = Point(charIndex, lineIndex)
+                    break
+                }
+                if (head.x != -1 && head.y != -1) {
+                    break
+                }
+            }
+        }
+
+        points.add(PointDirection(head, dir))
+        while (true) {
+            val destination = Point(head.x + dir.x, head.y + dir.y)
+            if (destination.x == -1 || destination.x == lines.size || destination.y == -1 || destination.y == lines.size) {
+                break
+            }
+            if (lines[destination.y][destination.x] == '#') {
+                // obstacle
+                dir = dir.next()
+            } else {
+                head = destination
+                points.add(PointDirection(head, dir))
+            }
+        }
+
+        val obstacles = points.map { it.point }.toSet().filter { runWalkAndCheckExist(it) }
+        println("Total distinct positions ${obstacles.size}")
     }
 }
