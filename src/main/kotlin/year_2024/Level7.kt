ce7cb7299014df9e1c71b725d582b8630718af2e
Ronny Remesnik <ronny.remesnik@autodesk.com>
2024-12-11 09:19:31 +0200

    Easy one, nice trick with concat

--- src/main/kotlin/year_2024/Level7.kt
+++ src/main/kotlin/year_2024/Level7.kt
@@ -3,6 +3,7 @@ package year_2024
 import Base2024
 
 class Level7 : Base2024(7) {
+
     enum class Options(val value: String) {
         Other(""),
         Add("+"),
@@ -13,6 +14,23 @@ class Level7 : Base2024(7) {
                 when (operator) {
                     '+' -> Add
                     '*' -> Multiply
+                    else -> Other
+                }
+        }
+    }
+
+    enum class Options2(val value: String) {
+        Other(""),
+        Add("+"),
+        Multiply("*"),
+        Concat("|");
+
+        companion object {
+            fun fromChar(operator: Char): Options2 =
+                when (operator) {
+                    '+' -> Add
+                    '*' -> Multiply
+                    '|' -> Concat
                     else -> Other
                 }
         }
@@ -41,6 +59,29 @@ class Level7 : Base2024(7) {
         }
     }
 
+    private fun generateOptions2(optionsCount: Int): List<String> {
+        return when (optionsCount) {
+            1, 0 -> {
+                emptyList()
+            }
+
+            2 -> {
+                Options2.values().filter { it.value.isNotEmpty() }.map { it.value }
+            }
+
+            else -> {
+                val list = generateOptions2(optionsCount - 1)
+                val updatedList: MutableList<String> = mutableListOf()
+                for (option in Options2.values().filter { it.value.isNotEmpty() }) {
+                    for (item in list) {
+                        updatedList.add(option.value + item)
+                    }
+                }
+                updatedList
+            }
+        }
+    }
+
     private fun calculateResult(option: String, items: List<Long>): Long {
         var total = items.first()
         val list = items.toMutableList()
@@ -56,10 +97,33 @@ class Level7 : Base2024(7) {
         return total
     }
 
+    private fun calculateResult2(option: String, items: List<Long>): Long {
+        var total = items.first()
+        val list = items.toMutableList()
+        list.removeFirst()
+        option.forEachIndexed { index, operator ->
+            when (Options2.fromChar(operator)) {
+                Options2.Add -> total += list[index]
+                Options2.Multiply -> total *= list[index]
+                Options2.Concat -> total = (total.toString() + list[index].toString()).toLong()
+                Options2.Other -> total
+            }
+        }
+
+        return total
+    }
+
     private fun checkResult(result: Long, items: List<Long>): Long {
         val options = generateOptions(items.size)
         return if (options.any {
                 calculateResult(it, items) == result
+            }) result else 0L
+    }
+
+    private fun checkResult2(result: Long, items: List<Long>): Long {
+        val options = generateOptions2(items.size)
+        return if (options.any {
+                calculateResult2(it, items) == result
             }) result else 0L
     }
 
@@ -74,5 +138,12 @@ class Level7 : Base2024(7) {
     }
 
     override fun part2() {
+        val sumOfGoodEquations = lines.sumOf { line ->
+            val result = line.substringBeforeLast(":").toLong()
+            val items = line.substringAfter(": ").split(" ").map { it.toLong() }
+            checkResult2(result, items)
+        }
+
+        println("The sum of good equations with concat is $sumOfGoodEquations")
     }
 }
